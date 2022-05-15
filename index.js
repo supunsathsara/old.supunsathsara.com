cbe5fbfcde3a2403739418cb91c715e89629fce9
Savindu Supun <savindusupunsathsara@gmail.com>
2022-05-16 02:47:47 +0530

    SW log commented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-if("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("sw.js").then(registration => {
 		console.log("SW Registerd");
-		console.log(registration);
+		//console.log(registration);
 	}).catch(erroe => {
 		console.log("SW registration Failed!");
 		console.log(error);
