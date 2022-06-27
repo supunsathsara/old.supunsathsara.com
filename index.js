8fe168500133b2ac398b4be1e9b8611aaddfc82d
Savindu Supun <savindusupunsathsara@gmail.com>
2022-06-27 22:09:44 +0530

    :star2: goingFullStack

--- index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("sw.js").then(registration => {
-		console.log("SW Registerd");
-		//console.log(registration);
-	}).catch(erroe => {
-		console.log("SW registration Failed!");
-		console.log(error);
-	});
-}
